8bcf886faed8ca8710e55774bb5bd2ecf82ec9b9
go_wt_flow <csw6115@gmail.com>
2026-06-04 15:39:01 +0900

    과제 제출

--- library/services/library_service.py
+++ library/services/library_service.py
@@ -14,21 +14,23 @@ class LibraryService(BaseService):
     """
 
     def __init__(self) -> None:
-        # TODO: 내부 리스트 초기화
-        pass
+        self._books : list[Book] = []
 
     def add_book(self, book: Book) -> None:
-        # TODO: 책 추가
-        raise NotImplementedError
+        self._books.append(book)
 
     def remove_book(self, title: str) -> None:
-        # TODO: 제목으로 책 삭제 (없으면 ValueError)
-        raise NotImplementedError
+        for idx, b in enumerate(self._books):
+            if b.title == title:
+                self._books.pop(idx)
+                return
+        raise ValueError
 
     def list_books(self) -> Iterable[Book]:
-        # TODO: 책 목록 반환 (복사본 반환 권장)
-        raise NotImplementedError
+        return self._books
 
     def find_book(self, title: str) -> Book:
-        # TODO: 제목으로 책 찾기 (없으면 ValueError)
-        raise NotImplementedError
+        for b in self._books:
+            if b.title == title:
+                return b
+        raise ValueError
